12e276adf86fed7105d6dc05c869ce7e3d025692
abrahamfergie <jhnfgie1989@gmail.com>
2019-04-30 10:17:07 -0700

    responsive formatting updates for accessibility

--- assets/javascript/script.js
+++ assets/javascript/script.js
@@ -1,17 +1,12 @@
 $(document).ready(function(){
-    const repoNames = [
-        {name: "Peak", deployedURL: "https://peak-af.herokuapp.com"}, 
-        {name: "React_BlackJack", deployedURL: "https://blackjack-af.herokuapp.com"}, 
-        {name: "myBooky", deployedURL: "https://bookstore-af.herokuapp.com"}, 
-        {name: "portfolio-af", deployedURL: "https://portfolio-af.herokuapp.com"} 
-    ]
+    $("#about-me-container")
+        .hide()
     $("#projects-container")
         .hide()
     $("#menu")
         .hide()
         .html("<div class='center'><h4 id='menu-close'>X Close</h4><h4 id='esc-key'>Esc</h4></div>")
-    $("#about-me-container")
-        .hide()
+    $(".carousel").carousel({interval: false})
     $(".menu-item").hover(function(){
         $(this).css("border-bottom", "2px solid #1c1c1c")
     }, function(){
@@ -32,33 +27,33 @@ $(document).ready(function(){
                 $(this).removeClass("fa-3x")
                 $(this).addClass("fa-2x")
             }
-        } else {
-            if($(this).hasClass("fa-1x")){
-                $(this).removeClass("fa-1x")
-            }else {
-                $(this).addClass("fa-1x")
-            }
-        }
+        } 
     })
-    if(window.outerWidth > 768) {
-        if(!$("#social-icon1").hasClass("fa-2x")){
-            $("#social-icon1, #social-icon2, #social-icon3").addClass("fa-2x")
-        }
-    } else {        
-        $("#social-icon1, #social-icon2, #social-icon3").removeClass("fa-2x")
-    }
+    $(".social-icons").on('touchstart', function(e) {
+        $(this).removeClass("fa-2x")
+        $(this).addClass("fa-3x")
+    })
+    $(".social-icons").on('touchend', function(e) {
+        $(this).removeClass("fa-3x")
+        $(this).addClass("fa-2x")
+    })
     $("#about-me").click(function() {
         $("#main-menu").fadeOut("fast")
-        $("#menu").fadeIn("fast")
-        if($("#about-me-container").children().length > 1){
-            $("#about-me-container").fadeIn(900)
-        }else {
-            $("#about-me-container").html("<h3>Hello World</h3>")        
-            $("#about-me-container").fadeIn(900)
-        }
+        $("#side-bar").fadeOut("fast")
+        $("#menu").fadeIn("slow")
+        $("#about-me-container").css({"display": "block"})
     })
+    const repoNames = [
+        {name: "Peak", deployedURL: "https://peak-af.herokuapp.com"}, 
+        {name: "React_BlackJack", deployedURL: "https://blackjack-af.herokuapp.com"}, 
+        {name: "myBooky", deployedURL: "https://bookstore-af.herokuapp.com"}, 
+        {name: "Quickness", deployedURL: "http://www.abrahamfergie.com/Quickness"},
+        {name: "portfolio-af", deployedURL: "https://portfolio-af.herokuapp.com"}
+    ]
     $("#projects").click(function(){
         $("#main-menu").fadeOut("fast")
+        $("#side-bar").fadeOut("fast")
+        window.outerWidth > 768 ? null : $("#social-media").fadeOut("fast")
         $("#menu").fadeIn("fast")
         $("#projects-container").css("position", "absolute")
         $("#projects-container").css("top", "10%")
@@ -70,26 +65,14 @@ $(document).ready(function(){
         }
         if(window.outerWidth < 768) {
             $("#social-media").fadeOut("fast")
-            $("#side-bar").css("height", "0%")
         }
     })
-    $("#icon").click(function(){
+    // $("#icon").click(function(){
 
-    })
+    // })
     $("#menu").click(menuEventHandler)
     $(document).keyup(menuEventHandler)
     $("#my-form-submit").click(sendEmail)
-
-    function menuEventHandler(event){
-        if(event.key === "Escape" || event.handleObj.type === "click"){
-            $("#menu").fadeOut("fast")
-            $("#about-me-container").hide()
-            $("#projects-container").fadeOut("fast")
-            $("#main-menu").show()
-            $("#side-bar").css("height", "100%")
-            window.outerWidth > 768 ? null : $("#social-media").fadeIn(900)
-        }
-    }
 })
 function getRepoInfo(repos) {
     $("#projects-container").fadeIn("fast")
@@ -109,15 +92,22 @@ function getRepoInfo(repos) {
         $.get("https://api.github.com/repos/abrahamfergie/" + repo.name, function(currentRepo){
             const projectsContainer = $("#projects-container")
             const row = $("<div class='row repos'>"),
-            col1 = $("<div class='center text-center'>"), 
-            col2 = $("<div class='center text-center'>"), 
-            col3 = $("<div class='center text-center'>")
+            col1 = $("<div class='center text-center info-wrapper-name'>"), 
+            col2 = $("<div class='center text-center info-wrapper'>"), 
+            col3 = $("<div class='center text-center info-wrapper'>")
             row.append(
-                col1.append("<strong>Repository Name:</strong>&nbsp" + currentRepo.name),
-                col2.append("<strong>Description:</strong>&nbsp" + currentRepo.description),
-                col3.append("<strong>URLS:</strong><a href='" + currentRepo.html_url + "' target='_blank'><u>" + "Check Out The Repo</u></a>"),
-                col3.append("<a href='" + repo.deployedURL + "' target='_blank'><u>" + "View The App</u></a>")
+                col1.append("<strong>Repository Name:</strong><h5>" + currentRepo.name + "</h5>"),
+                col2.append("<strong>Description:</strong>" + currentRepo.description),
+                col3.append(
+                    "<strong>URLS:</strong><div class='link-space'><a class='link' href='" +
+                    currentRepo.html_url + "' target='_blank'>" +
+                    "Check Out The Repo</a><a class='link' href='" +
+                    repo.deployedURL + "' target='_blank'>" + "View The App</a></div>"
+                )
             )
+            if(isMobile()){
+                fixColumns([col1, col2, col3])
+            }
             projectsContainer.append(row)
             getRepoInfo(repos)
         })
@@ -144,8 +134,19 @@ function menuEventHandler(event){
     if(event.key === "Escape" || event.handleObj.type === "click"){
         $("#menu").hide()
         $("#projects-container").hide()
-        $("#about-me-container").empty().hide()
-        $("#about-me").fadeIn("slow")
-        $("#projects").fadeIn("slow")
+        $("#about-me-container").hide()
+        $("#main-menu").fadeIn("slow")
+        $("#side-bar").fadeIn("slow")
+        $("#social-media").fadeIn("slow")
     }
+}
+function fixColumns(arr){
+    arr.forEach((col)=> {
+        col.removeClass("d-block")
+        col.addClass("d-flex")
+    })
+}
+function isMobile() {
+    try{ document.createEvent("TouchEvent"); return true; }
+    catch(e){ return false; }
 }
